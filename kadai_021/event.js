a404e0c57dd23aac25774a8f34baaeaf01190d78
w000nab0 <w000nab0@gmail.com>
2025-04-22 19:23:26 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+
+  setTimeout(() => {
+  text.textContent = 'ボタンをクリックしました'
+  }, 2000);
+
+});
+
+
+
